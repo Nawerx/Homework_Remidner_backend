af8d94c36d5211ed98b4aa92e40badac3a431486
Vadym <vdtrunov007@gmail.com>
2023-10-28 00:48:15 +0300

    Created login and registration View

--- Backend_drf/views.py
+++ Backend_drf/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
+from django.contrib.auth import login
+
+from .serializers import LoginSerializer, RegisterSerializer
 # Create your views here.
+
+
+class UserRegisterView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.create(request.data)
+            if user:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLoginView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.check_user(request.data)
+            login(request, user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
