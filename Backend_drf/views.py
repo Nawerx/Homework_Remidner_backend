75ee2f52bda1480da104608c64dfb75f9a520d51
Vadym <vdtrunov007@gmail.com>
2023-10-31 20:37:32 +0200

    created documentation for register and login view

--- Backend_drf/views.py
+++ Backend_drf/views.py
@@ -1,14 +1,34 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework_swagger.views import get_swagger_view
 
 from django.contrib.auth import login
 
 from .serializers import LoginSerializer, RegisterSerializer
+
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 # Create your views here.
 
 
+
 class UserRegisterView(APIView):
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'email': openapi.Schema(type=openapi.TYPE_STRING, format="email"),
+                'password': openapi.Schema(type=openapi.TYPE_STRING, format='password'),
+                'first_name': openapi.Schema(type=openapi.TYPE_STRING),
+                'last_name': openapi.Schema(type=openapi.TYPE_STRING),
+            }
+        ),
+        responses={
+            201: 'Created',
+            400: 'Bad Request'
+        }
+    )
     def post(self, request, *args, **kwargs):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
@@ -19,6 +39,19 @@ class UserRegisterView(APIView):
 
 
 class UserLoginView(APIView):
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'email': openapi.Schema(type=openapi.TYPE_STRING, format="email"),
+                'password': openapi.Schema(type=openapi.TYPE_STRING, format='password'),
+            }
+        ),
+        responses={
+            200: 'OK',
+            400: 'Bad Request',
+        }
+    )
     def post(self, request, *args, **kwargs):
         serializer = LoginSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
