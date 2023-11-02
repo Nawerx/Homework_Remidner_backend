c6626ea4aa025989295983d2eac96967640921c0
Vadym <vdtrunov007@gmail.com>
2023-11-02 21:28:36 +0200

    Added nested router

--- Backend_drf/urls.py
+++ Backend_drf/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from .views import UserLoginView, UserRegisterView, UserTasksViewSet, UserViewSet
+from rest_framework_nested import routers
 
-from .views import UserLoginView, UserRegisterView
+router = routers.SimpleRouter()
+router.register(r"users", UserViewSet)
+
+nested_router = routers.NestedSimpleRouter(router, "users", lookup="author")
+nested_router.register(r"task", UserTasksViewSet, basename="task")
 
 urlpatterns = [
     path("login/", UserLoginView.as_view()),
     path("signup/", UserRegisterView.as_view()),
+    path("", include(router.urls)),
+    path("", include(nested_router.urls)),
 ]
