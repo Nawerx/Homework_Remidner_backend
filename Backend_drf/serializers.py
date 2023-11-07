877f7b6d280c65f2012c1b81ddd253a0e38e929b
Vadym <vdtrunov007@gmail.com>
2023-11-08 01:18:08 +0200

    configured login and signup serializers, views, urls for knox

--- Backend_drf/serializers.py
+++ Backend_drf/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from django.contrib.auth import authenticate
 from .models import User, Task
+from django.utils.translation import gettext_lazy as _
+
+from rest_framework import serializers
 
 
 class SimpleTaskSerializer(serializers.ModelSerializer):
@@ -21,23 +24,48 @@ class UserSerializer(serializers.ModelSerializer):
         read_only_fields = ["id"]
 
 
-class LoginSerializer(serializers.Serializer):
-    email = serializers.CharField()
-    password = serializers.CharField()
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField(
+        label=_("Username"),
+        write_only=True
+    )
+    password = serializers.CharField(
+        label=_("Password"),
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+        write_only=True
+    )
+    token = serializers.CharField(
+        label=_("Token"),
+        read_only=True
+    )
 
-    def check_user(self, validated_data):
-        user = authenticate(
-            email=validated_data["email"], password=validated_data["password"]
-        )
-        if not user:
-            raise serializers.ValidationError("User not found")
-        return user
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        if email and password:
+            user = authenticate(request=self.context.get('request'),
+                                email=email, password=password)
+
+            if not user:
+                msg = _('Unable to log in with provided credentials.')
+                raise serializers.ValidationError(msg, code='authorization')
+        else:
+            msg = _('Must include "username" and "password".')
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
 
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["email", "password", "first_name", "last_name"]
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
 
     def create(self, validated_data):
         user = User.objects.create_user(email=validated_data["email"], password=validated_data["password"],
